38c32a17f877751a728f2cbbd57cd4fa0b7751ba
rgulersen <rgulersen@gmail.com>
2020-05-20 22:18:06 +0300

    fix  NotSupportedException  and if block (code quality)

--- aspnetcore-basic-architecture/Infrastructure/Filters/ValidationFilter.cs
+++ aspnetcore-basic-architecture/Infrastructure/Filters/ValidationFilter.cs
@@ -1,21 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 
 namespace AspnetCoreBasicArchitecture.Infrastructure.Filters
 {
     public class ValidationFilter : IActionFilter
     {
-       
-
         public void OnActionExecuting(ActionExecutingContext context)
         {
-            if (!context.ModelState.IsValid) context.Result = new BadRequestObjectResult(context.ModelState);
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
         }
-
 
         public void OnActionExecuted(ActionExecutedContext context)
         {
-           
+            throw new NotSupportedException();
         }
 
     }
